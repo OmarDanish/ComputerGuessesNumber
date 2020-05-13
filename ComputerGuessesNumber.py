0a9c946b1b40cc4c2062a69847ffbf0624a3b60f
OmarDanish <65194488+OmarDanish@users.noreply.github.com>
2020-05-13 05:10:16 -0500

    Add files via upload

---
+++ ComputerGuessesNumber.py
@@ -0,0 +1,28 @@
+print ("      ")
+\
+
+y=int(input("\nChoose a lower limit for the range that the computer guesses within\n"))
+x=int(input("\nChoose an upper limit for the range that the computer guesses within\n"))
+attempts=0
+
+guess=(x+y)/2
+
+while True:
+    if x < y:
+        print ("Lower limit cannot be be greater than upper limit")
+        print ("Restart the game with appropriate limits")
+        break
+    elif x > y:
+        print ("Think of a number and tell me if my guess is right or not\n")
+        print ("Is your number {}?".format(str(guess))
+        user_response = input("Yes/No:\n")
+        if user_response == "Yes":
+            print ("I got it!")
+            break
+        elif user_response == "No":
+            print("Was I too high or too low?")
+            range_response = input("Too high/too low:\n")
+            if range_response == "Too high":
+                x=guess
+            elif range_response == "Too low":
+                 y=x
